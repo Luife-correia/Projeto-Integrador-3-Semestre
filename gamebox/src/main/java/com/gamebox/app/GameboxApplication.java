da550ba1fc2fe19dbddd91248d71f3fa9758439c
Luiz Felipe Correia <lfc291006@gmail.com>
2026-09-19 15:04:30 -0300

    Commit inicial do back-end.

---
+++ gamebox/src/main/java/com/gamebox/app/GameboxApplication.java
@@ -0,0 +1,13 @@
+package com.gamebox.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameboxApplication.class, args);
+	}
+
+}
---
+++ gamebox/src/test/java/com/gamebox/app/GameboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamebox.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
