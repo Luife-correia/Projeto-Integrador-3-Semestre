240c41e21a2255b6463edc9348f6657dc427260b
LUCIANO LUIS DE SOUSA JUNIOR <luciano.lsjunior1@senacsp.edu.br>
2026-09-24 20:58:26 -0300

    Domain feita

--- gamebox/src/main/java/com/gamebox/app/Domain/Usuario.java
+++ gamebox/src/main/java/com/gamebox/app/Domain/Usuario.java
@@ -1,4 +1,38 @@
 package com.gamebox.app.Domain;
 
+import jakarta.persistence.*;
+
+@Table(name = "Usuarios")
+@Entity
 public class Usuario {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+
+    public Usuario(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
