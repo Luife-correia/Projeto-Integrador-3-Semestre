648c8117f8e20844319c302f384e2f898fa58ad4
LUCIANO LUIS DE SOUSA JUNIOR <luciano.lsjunior1@senacsp.edu.br>
2026-09-24 21:48:27 -0300

    Swagger implemantado para tests.

--- gamebox/src/main/java/com/gamebox/app/Service/UserService.java
+++ gamebox/src/main/java/com/gamebox/app/Service/UserService.java
@@ -1,4 +1,14 @@
 package com.gamebox.app.Service;
 
+import com.gamebox.app.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
