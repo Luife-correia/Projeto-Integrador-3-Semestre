02bfe16c310ef40f261339cba74be740002a0cf1
LUCIANO LUIS DE SOUSA JUNIOR <luciano.lsjunior1@senacsp.edu.br>
2026-09-24 21:31:07 -0300

    Tabela de usuarios feita.

--- gamebox/src/main/java/com/gamebox/app/Domain/Usuario.java
+++ gamebox/src/main/java/com/gamebox/app/Domain/Usuario.java
@@ -1,6 +1,10 @@
 package com.gamebox.app.Domain;
 
+import com.gamebox.app.Enum.Role;
 import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
 
 @Table(name = "Usuarios")
 @Entity
@@ -12,18 +16,56 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @NotNull
+    @Column(unique = true)
+    private String email;
 
-    public Usuario(Long id, String nome) {
+    @NotNull
+    private String senhaHash;
+
+    private String fotoPerfil;
+
+    private String bio;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private Date dataCriacao;
+
+    private boolean ativo;
+
+    /**
+     ********** Constructor **********
+     */
+
+    public Usuario(
+            Long id,
+            String nome,
+            String email,
+            String senhaHash,
+            Role role,
+            Date dataCriacao,
+            boolean ativo
+    ) {
         this.id = id;
         this.nome = nome;
+        this.email = email;
+        this.senhaHash = senhaHash;
+        this.role = role;
+        this.dataCriacao = dataCriacao;
+        this.ativo = ativo;
     }
+
+    /**
+     ********** GET E SET  **********
+     */
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -31,8 +73,56 @@ public class Usuario {
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+    public void setSenhaHash(String senhaHash) {
+        this.senhaHash = senhaHash;
+    }
+
+    public String getFotoPerfil() {
+        return fotoPerfil;
+    }
+    public void setFotoPerfil(String fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
     }
 }
