4a2c39b0e0dffe6596c54f184d8ddedbc9b47659
LUIZ FELIPE CORREIA DE SOUZA <luiz.fcsouza11@senacsp.edu.br>
2026-09-24 22:12:53 -0300

    Feature: Jogo implementado

--- gamebox/src/main/java/com/gamebox/app/Controller/JogoController.java
+++ gamebox/src/main/java/com/gamebox/app/Controller/JogoController.java
@@ -1,4 +1,21 @@
 package com.gamebox.app.Controller;
 
+import com.gamebox.app.Domain.Jogo;
+import com.gamebox.app.Service.JogoService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/jogos")
 public class JogoController {
+
+    private final JogoService jogoService;
+
+    public JogoController(JogoService jogoService) {
+        this.jogoService = jogoService;
+    }
+
+    @PostMapping
+    public Jogo criar(@RequestBody Jogo jogo) {
+        return jogoService.salvar(jogo);
+    }
 }
--- gamebox/src/main/java/com/gamebox/app/Service/JogoService.java
+++ gamebox/src/main/java/com/gamebox/app/Service/JogoService.java
@@ -1,4 +1,22 @@
 package com.gamebox.app.Service;
 
+import com.gamebox.app.Domain.Jogo;
+import com.gamebox.app.Repository.JogoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JogoService {
+
+    private final JogoRepository jogoRepository;
+
+    public JogoService(JogoRepository jogoRepository) {
+        this.jogoRepository = jogoRepository;
+    }
+
+    public Jogo salvar(Jogo jogo) {
+        return jogoRepository.save(jogo);
+    }
+
+
+
 }
